16287d4c76db78ae6605949c6bfcdb71a42b1c2c
Aaliyah Vargas <aaliyahv0186@hstat.org>
2021-03-22 14:36:56 +0000

    update repo

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ $.ajax({
     success: function(data) { 
         var userName = prompt("Find a popular baby name!, input any first letter and we'll give you a list of popular names the begin with that letter!");
         userName = "" //the input value or first 
-
+        
 for(var i = 0; i < data.length; i++) {
     if(data[i].nm.substr(userName) == 0) { 
 document.write("Here are all names beginning with" + userName + (data[i].nm)); 
+    
+        
+        }
         }
         }
         }//prints out names that based off the user input 
